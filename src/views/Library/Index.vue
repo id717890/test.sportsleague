37e81afd3051cbb99e205de5c44f22e9305c8ee8
Zamir Yusupov <jusupovz@gmail.com>
2021-08-22 13:22:45 +0500

    add sort and search functions

--- src/views/Library/Index.vue
+++ src/views/Library/Index.vue
@@ -1,10 +1,28 @@
 <template>
-  <div class="container pt-4 d-flex flex-grow-1">
+  <div class="container pt-4 d-flex flex-column flex-grow-1">
     <LoadingElement v-if="loading" />
-    <div class="row" v-else>
-      <LibraryBlock v-for="item in items" :item="item" :key="item._id" />
-      <Observer @intersect="intersected" />
-    </div>
+    <template v-else>
+      <div class="row mb-4">
+        <div class="col-12 col-sm-12 col-md-6">
+          <input
+            v-model="search"
+            class="form-control mb-2"
+            placeholder="Поиск по название и адресу"
+          />
+        </div>
+        <div class="col-12 col-sm-12 col-md-6">
+          <select class="form-select" v-model="sort" placeholder="Сортировать">
+            <option value="default">Сортировать по...</option>
+            <option value="name">По названию</option>
+            <option value="address">По адресу</option>
+          </select>
+        </div>
+      </div>
+      <div class="row">
+        <LibraryBlock v-for="item in libraries" :item="item" :key="item._id" />
+        <Observer @intersect="intersected" />
+      </div>
+    </template>
   </div>
 </template>
 
@@ -21,19 +39,76 @@ export default {
     LibraryBlock,
     LoadingElement,
   },
+  data: () => ({
+    search: null,
+    sort: 'default',
+  }),
   computed: {
     ...mapState({
       loading: (state) => state.loading,
       items: (state) => state.fetchedLibraries,
     }),
+    libraries() {
+      if (!this.search) {
+        return this.sortFunction(this.items)
+      } else {
+        const result = this.items?.filter(
+          (lib) =>
+            lib?.data?.general?.name?.toLowerCase().indexOf(this.search) !==
+              -1 ||
+            lib?.data?.general?.address?.fullAddress
+              ?.toLowerCase()
+              .indexOf(this.search) !== -1
+        )
+        const sortData = this.sortFunction(result)
+        return sortData
+      }
+    },
   },
   methods: {
     ...mapActions([types.INCREASE_PAGE_ACTION]),
     async intersected() {
       const part = await this[types.INCREASE_PAGE_ACTION]()
     },
+    sortFunction(data) {
+      let result
+      switch (this.sort) {
+        case 'name':
+          result = data.sort((a, b) => {
+            if (
+              a?.data?.general?.name?.toLowerCase() >
+              b?.data?.general?.name?.toLowerCase()
+            )
+              return 1
+            else if (
+              a?.data?.general?.name?.toLowerCase() <
+              b?.data?.general?.name?.toLowerCase()
+            )
+              return -1
+            else return 0
+          })
+          break
+        case 'address':
+          result = data.sort((a, b) => {
+            if (
+              a?.data?.general?.address?.fullAddress?.toLowerCase() >
+              b?.data?.general?.address?.fullAddress?.toLowerCase()
+            )
+              return 1
+            else if (
+              a?.data?.general?.address?.fullAddress?.toLowerCase() <
+              b?.data?.general?.address?.fullAddress?.toLowerCase()
+            )
+              return -1
+            else return 0
+          })
+          break
+        default:
+          return data
+      }
+      return result
+    },
     openDetails(item) {
-      console.log(item)
       this.$router.push({ name: 'LibraryItem', params: { id: item._id } })
     },
   },
